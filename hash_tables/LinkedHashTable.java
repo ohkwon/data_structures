72ab6a58c95c7d3361bd03ece93ced9843c6bf4b
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-12-20 21:30:26 -0800

    start practice on linked hash table"

---
+++ hash_tables/LinkedHashTable.java
@@ -0,0 +1,18 @@
+class LinkedHashTable {
+
+}
+
+public class LinkedList {
+  public class Node {
+    public String word;
+    public Node next;
+
+    public Node(String word) {
+
+    }
+  } 
+}
+
+public class HashTable {
+
+}
